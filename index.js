d7bf48f0bcb945fa05b518c55283b59272231863
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-09-26 10:16:51 +0530

    signup procedure done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 console.log("initial commit");
-//some configuration in master branch like listening 
+//some configuration in master branch like listening
+
+async function getDetails() {
+  return new Promise((resolve, reject) => {
+    const name = "amjad";
+    const email = "amjad@gmail.com";
+    const password = "123456";
+    resolve({ name, email, password });
+  });
+}
+
+async function signup() {
+  const { name, email, password } = await getDetails();
+  console.log(name, email, password);
+}
+
+
+signup();
