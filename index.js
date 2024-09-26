dd919e4c1c200f2bc976d8d9a371233cd5b33710
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-09-26 10:54:01 +0530

    summary added

--- index.js
+++ index.js
@@ -15,29 +15,29 @@ async function signup() {
   console.log(name, email, password);
 }
 
-
 signup();
-
-
-
 
 //after testing the all code of the feature branch i need to merge this feature branch to develop
 
+async function login() {
+  let email = "some@gmail.com";
+  let pass = "123456";
 
-async function login (){
-
-   let email = 'some@gmail.com'
-   let pass = '123456'
-
-   return new Promise((resolve, reject) => {
-       if (email == 'some@gmail.com' && pass == '123456'){
-           resolve({email, pass})
-       }else{
-           reject('invalid')
-       }
-   })
+  return new Promise((resolve, reject) => {
+    if (email == "some@gmail.com" && pass == "123456") {
+      resolve({ email, pass });
+    } else {
+      reject("invalid");
+    }
+  });
 }
 
 //after testing the all code of the feature branch i need to merge this feature branch to develop
+
+async function summary() {
+  console.log(
+    "iam making some summary in here for resolving the github thinks"
+  );
+}
 
 
